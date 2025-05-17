1a8f53f172efab419a79c2f24b0ab66353ae7f29
Sakhawat Hossain <hsakhawat135@gmail.com>
2025-05-18 05:46:50 +0600

    first commit

---
+++ apps/api/views.py
@@ -0,0 +1,22 @@
+# from django.shortcuts import render
+# from django.http import JsonResponse
+from apps.students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def StudentsView(request):
+    if request.method == 'GET':
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
---
+++ project/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #Web Application Endpoint
+    path('students/', include('apps.students.urls')),
+
+    #Api Endpoint
+    path('api/v1/', include('apps.api.urls'))
+]
